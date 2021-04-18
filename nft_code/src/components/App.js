f9b4dfc5a2db5ff17591de87d0c0bcb5afa98ef9
rajaka111 <Raje.Alharthi@gmail.com>
2021-04-18 05:58:10 +0300

    front end add

--- nft_code/src/components/App.js
+++ nft_code/src/components/App.js
@@ -75,7 +75,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://github.com/rajika111/NFT-Project"
             target="_blank"
             rel="noopener noreferrer"
           >
