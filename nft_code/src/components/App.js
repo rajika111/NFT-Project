f15b7f7aff4805d5e613f88878cb05660b66d0ec
rajaka111 <Raje.Alharthi@gmail.com>
2021-04-19 00:00:05 +0300

    fixed react error

--- nft_code/src/components/App.js
+++ nft_code/src/components/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import Web3 from 'web3'
 import './App.css';
 import Art from '../abis/Art.json'
@@ -14,11 +16,9 @@ class App extends Component {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
       await window.ethereum.enable()
-    }
-    else if (window.web3) {
+    } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
+    } else {
       window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   }
@@ -27,17 +27,23 @@ class App extends Component {
     const web3 = window.web3
     // Load account
     const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    this.setState({
+      account: accounts[0]
+    })
 
     const networkId = await web3.eth.net.getId()
     const networkData = Art.networks[networkId]
-    if(networkData) {
+    if (networkData) {
       const abi = Art.abi
       const address = networkData.address
       const contract = new web3.eth.Contract(abi, address)
-      this.setState({ contract })
+      this.setState({
+        contract
+      })
       const totalSupply = await contract.methods.totalSupply().call()
-      this.setState({ totalSupply })
+      this.setState({
+        totalSupply
+      })
       // Load Arts
       for (var i = 1; i <= totalSupply; i++) {
         const art = await contract.methods.arts(i - 1).call()
@@ -51,12 +57,16 @@ class App extends Component {
   }
 
   mint = (art) => {
-    this.state.contract.methods.mint(art).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({
-        arts: [...this.state.arts, art]
-      })
-    })
+    if (this.state.contract) {
+      this.state.contract.methods.mint(art).send({
+          from: this.state.account
+        })
+        .once('receipt', (receipt) => {
+          this.setState({
+            arts: [...this.state.arts, art]
+          })
+        })
+    }
   }
 
   constructor(props) {
@@ -70,61 +80,90 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="https://github.com/rajika111/NFT-Project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Art Tokens -Coding Dojo - Satoshi Team
-          </a>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white"><span id="account">{this.state.account}</span></small>
-            </li>
-          </ul>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <h1>Issue your Art Token</h1>
-                <form onSubmit={(event) => {
-                  event.preventDefault()
-                  const art = this.art.value
-                  this.mint(art)
-                }}>
-                  <input
-                    type='text'
-                    className='form-control mb-1'
-                    placeholder='e.g. #FFFFFF'
-                    ref={(input) => { this.art = input }}
-                  />
-                  <input
-                    type='submit'
-                    className='btn btn-block btn-primary'
-                    value='MINT'
-                  />
-                </form>
-              </div>
-            </main>
-          </div>
-          <hr/>
-          <div className="row text-center">
-            { this.state.arts.map((art, key) => {
-              return(
-                <div key={key} className="col-md-3 mb-3">
-                  <div className="token" style={{ backgroundArt: art }}></div>
-                  <div>{art}</div>
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </div>
+    return ( <
+      div >
+      <
+      nav className = "navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow" >
+      <
+      a className = "navbar-brand col-sm-3 col-md-2 mr-0"
+      href = "https://github.com/rajika111/NFT-Project"
+      target = "_blank"
+      rel = "noopener noreferrer" >
+      Art Tokens - Coding Dojo - Satoshi Team <
+      /a> <
+      ul className = "navbar-nav px-3" >
+      <
+      li className = "nav-item text-nowrap d-none d-sm-none d-sm-block" >
+      <
+      small className = "text-white" > < span id = "account" > {
+        this.state.account
+      } < /span></small >
+      <
+      /li> <
+      /ul> <
+      /nav> <
+      div className = "container-fluid mt-5" >
+      <
+      div className = "row" >
+      <
+      main role = "main"
+      className = "col-lg-12 d-flex text-center" >
+      <
+      div className = "content mr-auto ml-auto" >
+      <
+      h1 > Issue your Art Token < /h1> <
+      form onSubmit = {
+        (event) => {
+          event.preventDefault()
+          const art = this.art.value
+          this.mint(art)
+        }
+      } >
+      <
+      input type = 'text'
+      className = 'form-control mb-1'
+      placeholder = 'e.g. #FFFFFF'
+      ref = {
+        (input) => {
+          this.art = input
+        }
+      }
+      /> <
+      input type = 'submit'
+      className = 'btn btn-block btn-primary'
+      value = 'MINT' /
+      >
+      <
+      /form> <
+      /div> <
+      /main> <
+      /div> <
+      hr / >
+      <
+      div className = "row text-center" > {
+        this.state.arts.map((art, key) => {
+          return ( <
+            div key = {
+              key
+            }
+            className = "col-md-3 mb-3" >
+            <
+            div className = "token"
+            style = {
+              {
+                backgroundArt: art
+              }
+            } > < /div> <
+            div > {
+              art
+            } < /div> <
+            /div>
+          )
+        })
+      } <
+      /div> <
+      /div> <
+      /div>
     );
   }
 }
